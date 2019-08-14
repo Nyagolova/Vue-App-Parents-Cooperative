0e29b11d08e342b38cbdbe997ca3efa13c19beed
Nyagolova <zlatina.nyagolova@gmail.com>
2019-08-14 20:01:17 +0300

    Added Story Component
    Story component for Home Page

--- src/components/Operative/Story.vue
+++ src/components/Operative/Story.vue
@@ -1,10 +1,57 @@
 <template>
+  <div>
+      <v-card 
+        :flat="flat"
+        :loading="loading"
+        :outlined="outlined"
+        :elevation="elevation"
+        :raised="raised"
+        :max-width="maxwidth"
+        :height="height"
+      >
+
+        <v-list-item>
+            <v-list-item-content>
+                <v-list-item-title class="headline">{{StoryTitle}}</v-list-item-title>
+                <v-list-item-subtitle>{{StoryDate}}</v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+
+        <v-img
+          v-if="media"
+          class="white--text"
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+        </v-img>
   
+        <v-card-text>
+            <span class="text--primary">
+                {{StoryText}}
+            </span>
+        </v-card-text>
+
+        <v-card-actions v-if="actions">
+          <v-btn outlined color="cyan">Прочети повече</v-btn>
+        </v-card-actions>
+      </v-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+    props: ['StoryDate','StoryTitle','StoryText','StoryPhoto'],
+    data: () => ({
+        flat: false,
+        media: true,
+        loading: false,
+        actions: true,
+        outlined: false,
+        elevation: undefined,
+        raised: false,
+        maxwidth: 900,
+        height: undefined,
+    }),
 }
 </script>
 
--- src/views/ViewHomePage.vue
+++ src/views/ViewHomePage.vue
@@ -1,10 +1,52 @@
 <template>
-  <div>Home Page</div>
+  <div>
+    <div>Home Page</div>
+
+
+
+
+    <v-container
+            class="pa-2"
+            fluid
+          >
+            <v-row>
+              <v-col  v-for="Story in StriesData" 
+                    :key="Story.StoryId" >
+
+                  <story 
+                    :StoryDate="Story.StoryDate"
+                    :StoryTitle="Story.StoryTitle"
+                    :StoryText="Story.StoryText"
+                    :StoryPhoto="Story.StoryPhoto"
+                    > 
+                  </story>
+
+                </v-col>
+                </v-row>
+    </v-container>
+
+  </div>
 </template>
 
 <script>
-export default {
 
+import Story from '@/components/Operative/Story.vue'
+
+export default {
+  data () {
+        return {
+            StriesData: [
+              {StoryDate: '18/02/2019 12:30', StoryTitle:'First Story',StoryText:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",StoryPhoto:'',StoryId:'1'},
+              {StoryDate: '20/05/2019 09:15', StoryTitle:'Second Story',StoryText:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",StoryPhoto:'',StoryId:'2'},
+              {StoryDate: '03/07/2019 17:45', StoryTitle:'Third Story',StoryText:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",StoryPhoto:'',StoryId:'3'},
+              {StoryDate: '20/05/2019 09:15', StoryTitle:'Second Story',StoryText:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",StoryPhoto:'',StoryId:'4'},
+              {StoryDate: '03/07/2019 17:45', StoryTitle:'Third Story',StoryText:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",StoryPhoto:'',StoryId:'5'},
+            ]
+        }
+  },
+  components: {
+    Story
+  }
 }
 </script>
 
