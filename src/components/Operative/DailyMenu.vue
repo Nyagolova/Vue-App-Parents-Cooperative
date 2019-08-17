6df41a8e80d221d300388bbfbea5885604a906c9
Nyagolova <zlatina.nyagolova@gmail.com>
2019-08-17 19:55:03 +0300

    Update component: DailyMenu
    Added dish description and showing more information on hover

--- src/components/Operative/DailyMenu.vue
+++ src/components/Operative/DailyMenu.vue
@@ -18,38 +18,84 @@
 
         </v-app-bar>
   
-        <v-container>
-            <v-card
-                v-for="(dish, i) in Dishes"
-                :key="i"
-                light
-                class="mt-2"
-                outlined
-            >
-                <v-list-item>
-                    <v-list-item-content class="align-self-start">
+        <v-menu
+            open-on-hover
+            offset-x
 
-                        <v-list-item-title 
-                            class="title mb-2 font-weight-regular"
-                            v-text="dish.dishTitle"
-                        >
-                        </v-list-item-title>
-                            
-                        <v-list-item-subtitle 
-                            v-text="dish.dishType"
-                        >
-                        </v-list-item-subtitle>
-                        
+            v-for="(dish, i) in Dishes"
+            :key="i"
+            light
+            class="mt-2"
+            outlined
+        >
+            <template v-slot:activator="{ on }">
+                <v-container v-on="on">
+                    <v-card>
+                        <v-list-item>
+                            <v-list-item-content class="align-self-start">
 
+                                <v-list-item-title 
+                                    class="title mb-2 font-weight-regular"
+                                    v-text="dish.dishTitle"
+                                >
+                                </v-list-item-title>
+                                    
+                                <v-list-item-subtitle 
+                                    v-text="dish.dishType"
+                                >
+                                </v-list-item-subtitle>
+                                
+
+                            </v-list-item-content>
+                
+                            <v-list-item-avatar size="80">
+                                <v-img :src="dish.dishImgSrc"></v-img>
+                            </v-list-item-avatar>
+                    
+                        </v-list-item>
+                    </v-card>
+                </v-container>
+            </template>
+
+
+            <v-card style="max-width: 300px">
+                <v-list>
+                    <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="headline">{{dish.dishTitle}}</v-list-item-title>
                     </v-list-item-content>
-        
-                    <v-list-item-avatar size="80">
-                        <v-img :src="dish.dishImgSrc"></v-img>
-                    </v-list-item-avatar>
-            
-                </v-list-item>
+                    </v-list-item>
+                    
+                    <v-divider></v-divider>
+                
+                    <v-row align="center" justify="center">
+                        <v-img
+                        :src="dish.dishImgSrc"
+                        aspect-ratio="1"
+                        class="grey lighten-2"
+                        max-width="300"
+                        max-height="150"
+                        ></v-img>
+                    </v-row>
+                    
+                    <v-list-item two-line>
+                        <v-list-item-content>
+                            <v-list-item-title class="title">Съставки:</v-list-item-title>
+                            {{dish.dishDescription}}
+                        </v-list-item-content>
+                    </v-list-item>
+                    
+                </v-list>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn text @click="menu = false">Затвори</v-btn>
+                </v-card-actions>
             </v-card>
-        </v-container>
+
+
+        </v-menu>
+
     </v-card>
 
 </template>
--- src/views/ViewDaliyMenu.vue
+++ src/views/ViewDaliyMenu.vue
@@ -39,18 +39,21 @@ export default {
                 dishImgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Muesli.jpg/220px-Muesli.jpg',
                 dishTitle: 'Овесени ядки',
                 dishType: 'Закуска',
+                dishDescription: 'Овесени ядки, кисело мляко, сушени плодове'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'http://assets.kulinaria.bg/attachments/pictures-images/0000/1918/MAIN-vegetarianska-musaka.jpg?1431936459',
                 dishTitle: 'Мусака',
                 dishType: 'Обяд',
+                dishDescription: 'кайма, картофи, лук, домати, кисело мляко, яйца, олио, червен пипер, чубрица'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x500.jpg',
                 dishTitle: 'Плодове',
                 dishType: 'Следобедна закуска',
+                dishDescription: 'ягоди, портокали, киви, боровинки, грозде'
             }
         ]
     }, {
@@ -60,18 +63,21 @@ export default {
                 dishImgSrc: 'https://www.dinneratthezoo.com/wp-content/uploads/2015/04/banana-pancakes-4.jpg',
                 dishTitle: 'Бананови палачинки',
                 dishType: 'Закуска',
+                dishDescription: 'яйца, банани, бакпулвер, ванилия'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'http://www.1001recepti.com/images/photos/recipes/size_5/pile-s-oriz-1-[1940].jpg',
                 dishTitle: 'Пиле с ориз',
                 dishType: 'Обяд',
+                dishDescription: 'пилешки бутчета, ориз, дафинов лист, черен пипер, морков, глава лук, сол, олио'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'http://prekrasna.bg/wp-content/uploads/2014/02/krekeri.jpg',
                 dishTitle: 'Крекери',
                 dishType: 'Следобедна закуска',
+                dishDescription: 'вода, зехтин, пълнозърнесто брашно, сол, сусам, мак'
             }
         ]
     }, {
@@ -81,18 +87,21 @@ export default {
                 dishImgSrc: 'http://cook4cook.com/wp-content/uploads/b/buterbrody-k-zavtraku/1.jpg',
                 dishTitle: 'Сандвичи',
                 dishType: 'Закуска',
+                dishDescription: 'хляб, крема сирене, домати, краставици, шунка'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'https://gotvach.bg/files/lib/600x350/kufteta-parjeni11.jpg',
                 dishTitle: 'Кюфтета',
                 dishType: 'Обяд',
+                dishDescription: 'кайма, лук, хляб, яйца, чубрица, ким, магданоз, олио, сол'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'https://gotvach.bg/files/lib/250x250/choco-pudding-coconut.jpg',
                 dishTitle: 'Крем какао',
                 dishType: 'Следобедна закуска',
+                dishDescription: 'прясно мляко, яйца, какао, брашно, захар'
             }
         ]
     }, {
@@ -102,18 +111,21 @@ export default {
                 dishImgSrc: 'https://gotvach.bg/files/lib/600x350/new_banica_sirene.jpg',
                 dishTitle: 'Баница',
                 dishType: 'Закуска',
+                dishDescription: 'точени кори, яйца, кисело мляко, сирене, олио'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'http://www.gotvetesmen.com/images/recipes/4557_82282.jpg',
                 dishTitle: 'Боб яхния',
                 dishType: 'Обяд',
+                dishDescription: 'боб, моркови, лук, пипер, домати, джоджен, червен пипер'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x500.jpg',
                 dishTitle: 'Плодове',
                 dishType: 'Следобедна закуска',
+                dishDescription: 'ягоди, портокали, киви, боровинки, грозде'
             }
         ]
     }, {
@@ -123,18 +135,21 @@ export default {
                 dishImgSrc: 'https://s.rozali.com/p/m/a/makaroni-sirene-25830-500x334.jpg',
                 dishTitle: 'Макарони със сирене',
                 dishType: 'Закуска',
+                dishDescription: 'макарони, сирене'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'https://www.az-jenata.bg/media/az-jenata/files/articles/640x480/75ae6d902a0c178f536eb89fc0ac32d5.jpg',
                 dishTitle: 'Пълнен пипер',
                 dishType: 'Обяд',
+                dishDescription: 'пипер, кайма, лук, морков, домат, ориз, червен пипер, яйца, брашно, кисело мляко, прясно мляко'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'https://theviewfromgreatisland.com/wp-content/uploads/2019/04/fruit-salad-with-creamy-coconut-poppy-seed-dressing-8503620-April-01-2019.jpg',
                 dishTitle: 'Плодова салата',
                 dishType: 'Следобедна закуска',
+                dishDescription: 'къпини, малини, киви, портокал, грозде, ябълка'
             }
         ]
     }, {
@@ -144,18 +159,21 @@ export default {
                 dishImgSrc: 'https://thumbs.dreamstime.com/z/bowl-healthy-muesli-milk-26241381.jpg',
                 dishTitle: 'Мюсли с прясно мляко',
                 dishType: 'Закуска',
+                dishDescription: 'мюсли, прясно мляко, захар, сушени плодове'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'http://www.supichka.com/files/images/1386/sarmi.jpg',
                 dishTitle: 'Зелеви сaрми',
                 dishType: 'Обяд',
+                dishDescription: 'кайма, ориз, доматен сок, червен пипер, олио, морков, лук, магданоз, чубрица, сол, черен пипер'
             },
             {
                 color: '#ffff62',
                 dishImgSrc: 'https://ezine.bg/files/lib/600x350/makaroni-sas-sirene.jpg',
                 dishTitle: 'Макарони на фурна',
                 dishType: 'Следобедна закуска',
+                dishDescription: 'макарони, яйца, краве масло, ванилия, прясно мляко, сирене, захар'
             }
         ]
     }]
