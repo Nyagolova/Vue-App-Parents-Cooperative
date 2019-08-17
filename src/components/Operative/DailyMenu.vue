63e97f697ac2a197acd9b187e10b667abb2bc865
Nyagolova <zlatina.nyagolova@gmail.com>
2019-08-17 18:52:32 +0300

    Updating component: DailyMenu
    Data structure changes and adding more data

--- src/components/Operative/DailyMenu.vue
+++ src/components/Operative/DailyMenu.vue
@@ -1,68 +1,62 @@
 <template>
   
-    <v-card
+    <v-card class="mt-2" outlined>
+
+        <v-app-bar dark elevation="0" color="pink">
+  
+            <v-toolbar-title >
+                <slot></slot>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+    
+            <v-btn icon v-if="userIsAuthenticated">
+                <v-icon >
+                    mdi-pencil
+                </v-icon>
+            </v-btn>
+
+        </v-app-bar>
+  
+        <v-container>
+            <v-card
+                v-for="(dish, i) in Dishes"
+                :key="i"
+                light
+                class="mt-2"
+                outlined
+            >
+                <v-list-item>
+                    <v-list-item-content class="align-self-start">
+
+                        <v-list-item-title 
+                            class="title mb-2 font-weight-regular"
+                            v-text="dish.dishTitle"
+                        >
+                        </v-list-item-title>
+                            
+                        <v-list-item-subtitle 
+                            v-text="dish.dishType"
+                        >
+                        </v-list-item-subtitle>
+                        
+
+                    </v-list-item-content>
         
-        class="mt-2"
-        outlined="true"
-    >
-      <v-app-bar dark elevation="0" color="pink">
-  
-        <v-toolbar-title >
-            <slot></slot>
-        </v-toolbar-title>
-
-        <v-spacer></v-spacer>
-  
-        <v-btn icon v-if="userIsAuthenticated">
-          <v-icon >mdi-pencil</v-icon>
-        </v-btn>
-
-      </v-app-bar>
-  
-      <v-container>
-        <v-card
-            v-for="(item, i) in items"
-            :key="i"
-            light
-            class="mt-2"
-             outlined="true"
-        >
-            <v-list-item two-line>
-            <v-list-item-content class="align-self-start">
-            <v-list-item-title 
-                class="title mb-2 font-weight-regular"
-                
-                v-text="item.title"
-            >
-            </v-list-item-title>
-                
-
-            <v-list-item-subtitle v-text="item.artist"></v-list-item-subtitle>
-                  
-
-            </v-list-item-content>
-  
-            <v-list-item-avatar
-                size="80"
-                tile
-            >
-                <v-img :src="item.src"></v-img>
-            </v-list-item-avatar>
-                
-
-            </v-list-item>
-              
-        </v-card>
-          <!-- </v-col>
-        </v-row> -->
-      </v-container>
+                    <v-list-item-avatar size="80">
+                        <v-img :src="dish.dishImgSrc"></v-img>
+                    </v-list-item-avatar>
+            
+                </v-list-item>
+            </v-card>
+        </v-container>
     </v-card>
 
 </template>
 
 <script>
 export default {
-    props: ['items']
+    props: ['Dishes']
 }
 </script>
 
--- src/views/ViewDaliyMenu.vue
+++ src/views/ViewDaliyMenu.vue
@@ -9,11 +9,11 @@
     <v-layout justify-center>
       <v-row >
         <v-col  cols="4"
-          v-for="(day, index) in weekdays"
+          v-for="(day, index) in Dishes"
           :key="index"
         > 
-          <daily-menu :items="items">
-            {{day}}
+          <daily-menu :Dishes="day.properties">
+            {{day.weekday}}
           </daily-menu>
         </v-col>
       </v-row>
@@ -32,26 +32,133 @@ export default {
   },
   data: () => ({
     weekdays: ['Понеделник','Вторник','Сряда','Четврътък','Петък','Събота'],
-    items: [
-      {
-        color: '#ffff62',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Muesli.jpg/220px-Muesli.jpg',
-        title: 'Овесени ядки',
-        artist: 'Закуска',
-      },
-      {
-        color: '#ffff62',
-        src: 'http://assets.kulinaria.bg/attachments/pictures-images/0000/1918/MAIN-vegetarianska-musaka.jpg?1431936459',
-        title: 'Мусака',
-        artist: 'Обяд',
-      },
-      {
-        color: '#ffff62',
-        src: 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x500.jpg',
-        title: 'Плодове',
-        artist: 'Следобедна закуска',
-      }
-    ],
+    Dishes: [{
+        "weekday": "Понеделник",
+        "properties": [{
+                color: '#ffff62',
+                dishImgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Muesli.jpg/220px-Muesli.jpg',
+                dishTitle: 'Овесени ядки',
+                dishType: 'Закуска',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'http://assets.kulinaria.bg/attachments/pictures-images/0000/1918/MAIN-vegetarianska-musaka.jpg?1431936459',
+                dishTitle: 'Мусака',
+                dishType: 'Обяд',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x500.jpg',
+                dishTitle: 'Плодове',
+                dishType: 'Следобедна закуска',
+            }
+        ]
+    }, {
+        "weekday": "Вторник",
+        "properties": [{
+                color: '#ffff62',
+                dishImgSrc: 'https://www.dinneratthezoo.com/wp-content/uploads/2015/04/banana-pancakes-4.jpg',
+                dishTitle: 'Бананови палачинки',
+                dishType: 'Закуска',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'http://www.1001recepti.com/images/photos/recipes/size_5/pile-s-oriz-1-[1940].jpg',
+                dishTitle: 'Пиле с ориз',
+                dishType: 'Обяд',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'http://prekrasna.bg/wp-content/uploads/2014/02/krekeri.jpg',
+                dishTitle: 'Крекери',
+                dishType: 'Следобедна закуска',
+            }
+        ]
+    }, {
+        "weekday": "Сряда",
+        "properties": [{
+                color: '#ffff62',
+                dishImgSrc: 'http://cook4cook.com/wp-content/uploads/b/buterbrody-k-zavtraku/1.jpg',
+                dishTitle: 'Сандвичи',
+                dishType: 'Закуска',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'https://gotvach.bg/files/lib/600x350/kufteta-parjeni11.jpg',
+                dishTitle: 'Кюфтета',
+                dishType: 'Обяд',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'https://gotvach.bg/files/lib/250x250/choco-pudding-coconut.jpg',
+                dishTitle: 'Крем какао',
+                dishType: 'Следобедна закуска',
+            }
+        ]
+    }, {
+        "weekday": "Четвъртък",
+        "properties": [{
+                color: '#ffff62',
+                dishImgSrc: 'https://gotvach.bg/files/lib/600x350/new_banica_sirene.jpg',
+                dishTitle: 'Баница',
+                dishType: 'Закуска',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'http://www.gotvetesmen.com/images/recipes/4557_82282.jpg',
+                dishTitle: 'Боб яхния',
+                dishType: 'Обяд',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x500.jpg',
+                dishTitle: 'Плодове',
+                dishType: 'Следобедна закуска',
+            }
+        ]
+    }, {
+        "weekday": "Петък",
+        "properties": [{
+                color: '#ffff62',
+                dishImgSrc: 'https://s.rozali.com/p/m/a/makaroni-sirene-25830-500x334.jpg',
+                dishTitle: 'Макарони със сирене',
+                dishType: 'Закуска',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'https://www.az-jenata.bg/media/az-jenata/files/articles/640x480/75ae6d902a0c178f536eb89fc0ac32d5.jpg',
+                dishTitle: 'Пълнен пипер',
+                dishType: 'Обяд',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'https://theviewfromgreatisland.com/wp-content/uploads/2019/04/fruit-salad-with-creamy-coconut-poppy-seed-dressing-8503620-April-01-2019.jpg',
+                dishTitle: 'Плодова салата',
+                dishType: 'Следобедна закуска',
+            }
+        ]
+    }, {
+        "weekday": "Събота",
+        "properties": [{
+                color: '#ffff62',
+                dishImgSrc: 'https://thumbs.dreamstime.com/z/bowl-healthy-muesli-milk-26241381.jpg',
+                dishTitle: 'Мюсли с прясно мляко',
+                dishType: 'Закуска',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'http://www.supichka.com/files/images/1386/sarmi.jpg',
+                dishTitle: 'Зелеви сaрми',
+                dishType: 'Обяд',
+            },
+            {
+                color: '#ffff62',
+                dishImgSrc: 'https://ezine.bg/files/lib/600x350/makaroni-sas-sirene.jpg',
+                dishTitle: 'Макарони на фурна',
+                dishType: 'Следобедна закуска',
+            }
+        ]
+    }]
   }),
 }
 </script>
