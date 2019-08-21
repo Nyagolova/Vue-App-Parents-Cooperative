eea3d6860f1be5482488687a25f8996faeacee5a
Nyagolova <zlatina.nyagolova@gmail.com>
2019-08-21 21:19:57 +0300

    Update component: AuthForm
    Added backend validation - showing login and registration errors

--- src/components/Authentication/AuthForm.vue
+++ src/components/Authentication/AuthForm.vue
@@ -33,17 +33,21 @@
 
                     <v-flex xs12 v-if="showRegister">
                         <v-text-field
-                            v-model="password" ref="password"
-                            name="password" :type="'password'"
-                            v-validate="'required|min:6'"
-                            :error-messages="errors.collect('password')"
-                            label="Password"
-                            data-vv-name="password"
+                            v-model="passwordSecond" ref="passwordSecond"
+                            name="passwordSecond" :type="'password'"
+                            v-validate="'required|min:6|confirmed:password'"
+                            :error-messages="errors.collect('passwordSecond')"
+                            label="Retype password"
+                            data-vv-name="passwordSecond"
                             required
                         ></v-text-field>
                     </v-flex>
 
-                    <v-flex xs12 centered v-if="!showRegister">
+                    <v-flex xs12 v-if="errorMsg">
+                        <big class="errorMsg">{{errorMsg}} </big>
+                    </v-flex>
+
+                    <v-flex xs12 v-if="!showRegister">
                         <big>Нямате акаунт? - <a @click="onShowRegister()"> регистрирайте се тук </a> </big>
                     </v-flex>
 
@@ -55,10 +59,13 @@
             <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn color="red darken-1" text @click.stop="onCancelClick()">Отказ</v-btn>
-
-                <v-btn v-if="showRegister" color="green darken-1" text @click.stop="onRegisterClick()">Регистрирай се</v-btn>
-                <v-btn v-else color="green darken-1" text @click.stop="onLoginClick()">Влез</v-btn>
+                <v-btn :disabled="hasErrors" v-if="showRegister" color="green darken-1" text @click.stop="onRegisterClick()">Регистрирай се</v-btn>
+                <v-btn :disabled="hasErrors" v-else color="green darken-1" text @click.stop="onLoginClick()">Влез</v-btn>
             </v-card-actions>
+
+            <v-overlay :value="overlay">
+                <v-progress-circular indeterminate size="64"></v-progress-circular>
+            </v-overlay>
 
         </v-card>
     </v-dialog>
@@ -79,8 +86,11 @@ export default {
     select: null,
             email: '',
             password: '',
+            passwordSecond: '',
             authMode: 'Вход',
-            showRegister: false
+            showRegister: false,
+            overlay: false,
+            errorMsg: ''
         }
     },
     props: {
@@ -94,41 +104,54 @@ export default {
             set (value) {
                 this.$emit('input', value)
             }
+        },
+        hasErrors () {
+            return this.errors.items.length !== 0 ;
         }
     },
     mixins : [authenticate],
     methods: {
         onCancelClick() {
+            this.email = ''
+            this.password = ''
+            this.passwordSecond = ''
             this.authMode = 'Вход'
             this.showRegister = false
             this.show = false
+            this.errorMsg = false
         },
         onLoginClick() {
+            this.overlay = true;
+            this.$validator.validateAll()
             this.login( this.email, this.password).then(
-                status => {
-                    console.log(status); 
-                    this.$validator.validateAll()
-                    //this.show = false;
-                }
+                status => this.authHandler(status)
             ) 
         },
         onRegisterClick() {
+            this.overlay = true;
+            this.$validator.validateAll()
             this.register( this.email, this.password).then(
-                status => {
-                    console.log('ended')
-                    console.log(status); this.show = false;
-                }
+                status => this.authHandler(status)
             ) 
         },
+        authHandler(status) {
+            this.overlay = false;
+            if(status.type === 'error') {
+                this.errorMsg = status.text;
+            } else {
+                this.show = false;
+            }
+        },
         onShowRegister () {
-            this.authMode = 'Регистрирация'
-            this.showRegister = true
+            this.authMode = 'Регистрирация';
+            this.showRegister = true;
         }   
     }
-    
 }
 </script>
 
-<style>
-
+<style scoped>
+.errorMsg {
+    color: red;
+}
 </style>
