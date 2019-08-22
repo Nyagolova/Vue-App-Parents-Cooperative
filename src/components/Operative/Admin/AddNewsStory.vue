831c3b9944fa694781d7e8182417b297c25b440c
Nyagolova <zlatina.nyagolova@gmail.com>
2019-08-22 22:03:03 +0300

    Update component: ViewPageAdmin
    Conditionaly rendering different admin actions

---
+++ src/components/Operative/Admin/AddNewsStory.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+      Add news story
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/ViewPageAdmin.vue
+++ src/views/ViewPageAdmin.vue
@@ -1,10 +1,47 @@
 <template>
-  <p>admin page</p>
+  <v-expansion-panels focusable>
+    <v-expansion-panel
+      v-for="(adminForm, i) in adminPanelForms"
+      :key="i"
+    >
+      <v-expansion-panel-header>{{adminForm.title}}</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <component v-bind:is="adminForm.componentName"></component>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
-export default {
 
+import AddCalendarEvent from '@/components/Operative/Admin/AddCalendarEvent'
+import AddMenuDish from '@/components/Operative/Admin/AddMenuDish'
+import AddNewsStory from '@/components/Operative/Admin/AddNewsStory'
+
+export default {
+  data () {
+    return {
+      adminPanelForms: [
+        {
+          title: 'Добави новина',
+          componentName: 'add-news-story'
+        },
+        {
+          title: 'Добави дейност в календара',
+          componentName: 'add-calendar-event'
+        },
+        {
+          title: 'Добави ястие в менюто',
+          componentName: 'add-menu-dish'
+        }
+      ]
+    }
+  },
+  components: {
+    AddCalendarEvent,
+    AddMenuDish,
+    AddNewsStory
+  }
 }
 </script>
     
