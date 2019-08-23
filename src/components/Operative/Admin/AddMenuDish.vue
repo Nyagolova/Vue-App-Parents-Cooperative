f1f3e3b1ddb41aeedc104b2d108d95d44da8ea0d
Zlatina Nyagolova <zlatina.nyagolova@gmail.com>
2019-08-23 13:44:16 +0300

    Update component: AddMenuDish
    Form created for adding dishes to the daily menu

--- src/components/Operative/Admin/AddMenuDish.vue
+++ src/components/Operative/Admin/AddMenuDish.vue
@@ -1,12 +1,76 @@
 <template>
   <v-container>
-      Add menu dish
+    <v-row>
+      <v-col>
+        <v-select
+          :items="daysOfTheWeek"
+          label="Ден от седмицата"
+        ></v-select>
+      </v-col>
+      <v-col>
+        <v-select
+          :items="dishType"
+          label="Тип"
+        ></v-select>
+      </v-col>
+    </v-row>
+
+
+    <v-row>
+      <v-col cols="8"> 
+          <v-text-field
+              v-model="dishTitle"
+              v-validate="'required|min:5'"
+              :error-messages="errors.collect('dishTitle')"
+              label="Наименование ястие"
+              data-vv-name="dishTitle"
+              required
+          ></v-text-field>
+      </v-col>
+
+      <v-col> 
+          <v-file-input
+              v-model="dishPhoto"
+              label="Качи снимка"
+              filled
+              accept="image/*"
+              prepend-icon="mdi-camera"
+          ></v-file-input>
+      </v-col>
+    </v-row>
+
+    <v-textarea 
+        outlined
+        v-model="dishDescription"
+        v-validate="'required|min:100'"
+        :error-messages="errors.collect('dishDescription')"
+        data-vv-name="dishDescription"
+        name="input-7-4"
+        label="Ястие описание"
+        value=""
+    ></v-textarea>
+
+    <v-btn outlined color="cyan" :disabled="false" @click="UploadStory()"> Добави ястие </v-btn>
+
+    <v-overlay :value="overlay">
+        <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
+
   </v-container>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      daysOfTheWeek: ['Понеделник', 'Вторник', 'Сряда', 'Четвъртък', 'Петък', 'Събота'],
+      dishType: ['Закуска', 'Обяд', 'Вечеря'],
+      dishTitle: '',
+      dishPhoto: null,
+      dishDescription: '',
+      overlay: false
+    }
+  }
 }
 </script>
 
